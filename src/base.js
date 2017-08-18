b3586d3587f5ad477f46ad848947abeb6b6710a6
Julius Suominen <julius.suominen@gmail.com>
2017-08-18 19:14:17 +0200

    video 21

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCth-nKr2r11dARfxYgopDSxVmY-O9RzAo",
+  authDomain: "catch-of-the-day-ec4ba.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-ec4ba.firebaseio.com"
+});
+
+export default base;
